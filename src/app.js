dac24f96f517762e23c672e8ae2a69a49c17ce69
DinkyKum <dinky2912@gmail.com>
2025-03-11 14:11:03 +0530

    Changed link

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ require('dotenv').config()
 const port = process.env.PORT
 
 app.use(cors({
-  origin:"farmxpress-7o5jnlikc-dinky-kumars-projects.vercel.app",
+  origin:"farmxpress.vercel.app",
   methods: "GET,POST,PATCH,PUT,DELETE,OPTIONS",
   credentials:true,
 }));
